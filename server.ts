25cb029afc14c5d62acb495020dc5061495ae337
Ian Langworth <ian@langworth.com>
2020-03-08 03:16:44 +0000

    it all works

--- server.ts
+++ server.ts
@@ -1,11 +1,18 @@
 import * as express from "express"
+import * as knex from "knex"
 
 const PORT = 3000
 const HOST = "0.0.0.0"
 
+const pg = knex({
+  client: "pg",
+  connection: "postgresql://user:pass@db/data"
+})
+
 const app = express()
-app.get("/", (req: any, res: any) => {
-  res.send("Hello remote world! 2\n")
+app.get("/", async (req: any, res: any) => {
+  const data = await pg.select("*").from("pg_config")
+  res.send(`Hello ${JSON.stringify(data, null, "  ")}`)
 })
 
 app.listen(PORT, HOST)
